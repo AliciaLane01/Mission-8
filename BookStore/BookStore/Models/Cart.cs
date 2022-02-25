33eeed5e37cb3acaebe544af1b73ac29ecf43eee
AliciaLane01 <62069491+AliciaLane01@users.noreply.github.com>
2022-02-24 22:51:35 -0700

    partial view, sort by category

---
+++ BookStore/BookStore/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Cart
+    {
+        public class CartItem
+        {
+            public int ItemID { get; set; }
+
+        }
+    }
+}
